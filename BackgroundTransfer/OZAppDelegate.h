9dc5deeb2d10516e946135ad761dfacdbbe243e8
Bas <bas@oozou.com>
2014-03-18 16:04:22 +0700

    Initial Commit

---
+++ BackgroundTransfer/OZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OZAppDelegate.h
+//  BackgroundTransfer
+//
+//  Created by Kiattisak Anoochitarom on 3/18/2557 BE.
+//  Copyright (c) 2557 Kiattisak Anoochitarom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BackgroundTransfer/OZViewController.h
@@ -0,0 +1,13 @@
+//
+//  OZViewController.h
+//  BackgroundTransfer
+//
+//  Created by Kiattisak Anoochitarom on 3/18/2557 BE.
+//  Copyright (c) 2557 Kiattisak Anoochitarom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OZViewController : UIViewController
+
+@end
